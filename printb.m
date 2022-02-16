a9a74ccdf251937b2d37558452c5379a94d923aa
Nicolas Kondós <60369117+nickkontos@users.noreply.github.com>
2022-02-16 23:13:35 +0200

    Update printb.m

--- printb.m
+++ printb.m
@@ -1,7 +1,7 @@
 function y = printb(b)
 
     btbl=cell(size(b,1)+1,size(b,2));
-    btbl{1,1} = sprintf('stock');
+    btbl{1,1} = sprintf('RHS'); %Right Hand Side of equation(s)
     for l=1:size(b,1)
         for k=1:size(b,2)
             btbl{l+1,k} = b(l,k);
